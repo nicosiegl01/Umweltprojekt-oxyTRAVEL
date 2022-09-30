b35f91aefde100b9caf06b9969d31d1f725283e0
Nico Siegl <sieglnico@gmail.com>
2022-09-30 19:02:19 +0200

    initial

---
+++ Projekt/Frontend/src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Frontend';
+
+  constructor() {
+    //const
+
+    console.log('test')
+
+    //oebb.searchStationsNew("Wien").then(console.log);
+  }
+}
