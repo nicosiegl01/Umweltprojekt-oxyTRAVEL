95a0f1dc49c8fb55a49fd94f48f825cf1016f606
miamandel <miasaramandel@gmail.com>
2022-11-21 12:39:14 +0100

    added some frontend

--- Projekt/Frontend/src/app/login/login.component.ts
+++ Projekt/Frontend/src/app/login/login.component.ts
@@ -11,6 +11,7 @@ export class LoginComponent implements OnInit {
   loginForm = new FormGroup({
     email: new FormControl('', [Validators.required, Validators.email]),
     password: new FormControl('', Validators.required),
+    
   });
 
   constructor(
