2d05813dcc30db858cc4bc35c1bf15b4d6019bd9
Nico Siegl <sieglnico@gmail.com>
2022-11-21 13:32:21 +0100

    update backend

---
+++ Projekt/Backend/src/main/java/at/htl/Endpoints/CustomerRessource.java
@@ -0,0 +1,37 @@
+package at.htl.Endpoints;
+
+import at.htl.Model.Customer;
+import at.htl.Repository.CustomerRepository;
+
+import javax.inject.Inject;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import java.util.List;
+
+@Path("/api")
+public class CustomerRessource {
+    @Inject
+    CustomerRepository customerRepository;
+
+    @GET
+    @Path("/getAll")
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<Customer> getAllCustomer(){
+        return customerRepository.listAll();
+    }
+
+    @GET
+    @Path("/getByUsername/{mail}/{password}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public boolean loginCustomer(@PathParam("mail") String mail,
+                                 @PathParam("password") String password){
+        return this.customerRepository.findByMail(mail, password);
+    }
+
+    @POST
+    @Path("/create")
+    @Consumes(MediaType.APPLICATION_JSON)
+    public void createCustomer(Customer c){
+        this.customerRepository.persist(c);
+    }
+}
